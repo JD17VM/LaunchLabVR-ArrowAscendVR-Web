506215ba591008104fe4a60bf4082d05844d9cca
JD17VM <jdiegodiego4@gmail.com>
2024-09-15 17:08:15 -0500

    :package: chore(dependencies): add managing image imports

---
+++ src/utils/imageHelper.jsx
@@ -0,0 +1,11 @@
+import Logo_IHC_normal from '../assets/imgs/Logos/Logo_IHC_Normal.png';
+import Logo_LaunchLab from '../assets/imgs/Logos/Logo_LaunchLab.png';
+import Logo_ArrowAscend_fondo_negro from '../assets/imgs/Logos/Logo_ArrowAscend_fondo_negro.png';
+
+const imageHelper = {
+    Logo_IHC_normal,
+    Logo_LaunchLab,
+    Logo_ArrowAscend_fondo_negro,
+}
+
+export default imageHelper;
