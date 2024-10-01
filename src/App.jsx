d2815c578d00dfa65a59473a47cb5df29d6ce154
JD17VM <jdiegodiego4@gmail.com>
2024-09-30 23:48:37 -0500

    :sparkles: feat: add Proyectos route

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Footer} from './widgets/Footer'
 import { Inicio} from './Inicio'
 import { Nosotros } from './Nosotros'
 import { Proceso } from './Proceso'
+import { Proyectos } from './Proyectos'
 
 function App() {
 
@@ -16,10 +17,12 @@ function App() {
           <Route path="/" element={<Inicio />} />
           <Route path="/nosotros" element={<Nosotros />} />
           <Route path="/proceso" element={<Proceso />} />
+          <Route path="/proyectos" element={<Proyectos />} />
           
           <Route path="/index2" element={<Inicio />} />
           <Route path="/nosotros2" element={<Nosotros />} />
           <Route path="/proceso2" element={<Proceso />} />
+          <Route path="/proyectos2" element={<Proyectos />} />
         </Routes>
       </main>
 
---
+++ src/Proyectos.jsx
@@ -0,0 +1,37 @@
+import { Cabecera } from './widgets/Cabecera'
+
+import { Link } from 'react-router-dom';
+
+import imageHelper from './utils/imageHelper'
+import { useEffect } from 'react';
+import styles from './assets/styles/estilos_proyectos.module.css'
+
+export function Proyectos() {
+    useEffect(() => {
+        document.title = "Proyectos";
+      }, []);
+    return (
+        <>
+            <Cabecera background_image={imageHelper.background_cabecera_proyectos}>Proyectos</Cabecera>
+            <div className={styles.contenido}>
+                <div className={styles.seccion}>
+                    <div className={styles.contenido_seccion}>
+                        <Link to="/" className={styles.cont_archivo} >
+                            <div>
+                                <img src={imageHelper.Logo_LaunchLab} alt=""/>
+                            </div>
+                            <p>Launch Lab VR</p>
+                        </Link>
+
+                        <Link to="/index2" className={styles.cont_archivo} >
+                            <div>
+                                <img src={imageHelper.Logo_ArrowAscend_fondo_negro} alt=""/>
+                            </div>
+                            <p>Arrow Ascend</p>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
